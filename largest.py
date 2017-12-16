05e51c91126c30de1594425746fee6de341e298f
devikamadasamy <34593992+devikamadasamy@users.noreply.github.com>
2017-12-16 13:56:05 +0530

    Update largest.py

--- largest.py
+++ largest.py
@@ -2,9 +2,8 @@ a=int(raw_input("enter the a value:\n"))
 b=int(raw_input("enter the b value:\n"))
 c=int(raw_input("enter the c value:\n"))
 if((a>b) and (a>c)):
-	print(str(a)+" is largest") 
+	print(a) 
 elif(c>b):
-	print(str(c)+" is largest")
+	print(c)
 else:
-	print(str(b)+" is greatesst")                                      
-  
+	print(b)                                      
