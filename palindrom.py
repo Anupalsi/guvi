c9d0b690d6db52158b3eb43b3a943f14b42cc704
devikamadasamy <34593992+devikamadasamy@users.noreply.github.com>
2017-12-20 22:27:00 +0530

    Update palindrom.py

--- palindrom.py
+++ palindrom.py
@@ -1,12 +1,11 @@
 num=int(raw_input("Enter a number\n"))
 sum1=0
 n=num
- 
 while num!=0:
 	rem=num%10
 	sum1=sum1*10+rem
 	num=num/10
 if sum1==n:
-	print n, "is palindrome"
+	print("palindrome")
 else:
-	print n, "is not palindrome"
+	print("not palindrome")
