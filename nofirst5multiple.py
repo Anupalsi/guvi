fa442166fc9385f62df82dd7a4a62db4d4a435fe
devikamadasamy <34593992+devikamadasamy@users.noreply.github.com>
2017-12-22 14:38:29 +0530

    Update nofirst5multiple.py

--- nofirst5multiple.py
+++ nofirst5multiple.py
@@ -1,5 +1,13 @@
 n=input("enter the no \n")
-sum=0
+sum=n-5
 for i in range(0,5):
-	sum=sum+5
-	print(sum)
+	if(n%5!=0):
+		e=n%5
+		g=5-e
+		n=g+n
+		sum=n-5
+		sum=sum+5
+		print(sum)
+	else:
+		sum=sum+5
+		print(sum)
