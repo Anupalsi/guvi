993c143f3bc0806cd0e841b6b621c1fa9508c3a9
devikamadasamy <34593992+devikamadasamy@users.noreply.github.com>
2018-06-12 12:06:03 +0530

    Create play_83.py

---
+++ play_83.py
@@ -0,0 +1,20 @@
+N=int(input())
+def b(m):
+    l="{0:b}".format(m)
+    return int(l)
+h=int(input("ënter the first no"))
+if N==1:
+    print(h)
+else:    
+ p=b(h)
+ if N<3:
+    m=int(input("ënter the 2 no"))
+    u=b(m)
+    p=p|u
+ else:
+    for i in range(2,N+1):
+        me=int(input("enter the"+str(i)+"no"))
+        u=b(me)
+        p=p|u
+ decimal = int(str(p), 2);
+ print(decimal)
