a168f4e66e875c6c0b8ba54610ffc6948b1f15b4
devikamadasamy <34593992+devikamadasamy@users.noreply.github.com>
2018-06-14 15:02:38 +0530

    Update play63.py

--- play63.py
+++ play63.py
@@ -3,9 +3,13 @@ a=input()
 b=input()
 aa=a.split(" ")
 bb=b.split(" ")
+a=set(aa)
+b=set(bb)
+n=len(a)
+m=len(b)
 list=[]
-for i in range(N):
-    for j in range(N):
+for i in range(n):
+    for j in range(m):
         if aa[i]==bb[j]:
             list.append(aa[j])
 s=" ".join(list)
