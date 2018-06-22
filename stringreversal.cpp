788a2d4ad56729fcb66a0a2374d8229a493d82bd
devikamadasamy <34593992+devikamadasamy@users.noreply.github.com>
2018-06-22 10:35:54 +0530

    Update stringreversal.cpp

--- stringreversal.cpp
+++ stringreversal.cpp
@@ -1,53 +1,55 @@
-# your code goes here
 #include <iostream>
 #include <vector>
 using namespace std;
-class reversal:{
-	vector<char> s;
+class reversal
+{
+	vector <char> s;
+   
+    public:
     vector <char> :: iterator itr;
-    public
     void push(char);
     int empty();
     char pop();
     
     
     };
-void reversal:push(char val)    
+void reversal::push(char val)    
 {
 	s.push_back(val);
 }
-int empty()
+int reversal::empty()
 {
 	if(s.empty())
-	return 0
+	return 0;
 	else
-	return 1
+	return 1;
 }
-char pop()
-{
+char reversal::pop()
+{ char m;
+//vector <char> :: iterator itr;
 	itr=--s.end();
     m=*itr;
+    //cout<<m;
     s.erase(itr);
     return m;
     
 }
 int main()
-{
+{int i;
 	char b[100],c[100];
 	reversal re;
 	cout<<"enter the string\n";
 	cin.get(b,100);
-	for(i=0;i!='\o';i++)
+	for(i=0;b[i]!='\0';i++)
 	{
 		re.push(b[i]);
 }
-for(i=0;i!='\o';i++)
+for(i=0;b[i]!='\0';i++)
 {
-	a=re.empty();
-	if(a==1)
+
 		c[i]=re.pop();
 	
 }
-cout<<c
+cout<<c;
 return 0;
 }
