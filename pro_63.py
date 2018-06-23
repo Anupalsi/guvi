3aa0a1d2467facf35fe1f6e967f759e35e1f1819
devikamadasamy <34593992+devikamadasamy@users.noreply.github.com>
2018-06-23 18:10:06 +0530

    Update pro_63.py

--- pro_63.py
+++ pro_63.py
@@ -1,4 +1,14 @@
 a=input()
 b=list(a)
-c=set(b)
-print(len(c))
+li=1
+for i in range(1,len(b)):
+	flag=0
+	list4=b[:i]
+	for j in range(0,len(list4)):
+		if b[i]!=list4[j]:
+			flag=flag+1
+	if flag==len(list4):
+		li=li+1
+	else:
+		break
+print(li)
