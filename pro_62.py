871d7a5d796f23e1755b922c0dcac98c61d2e677
devikamadasamy <34593992+devikamadasamy@users.noreply.github.com>
2018-06-23 16:49:02 +0530

    Create pro_62.py

---
+++ pro_62.py
@@ -0,0 +1,21 @@
+from itertools import combinations
+l=input()
+m=list(l)
+list1=[]
+for i in range(2,len(m)+1):
+	comb=combinations(m,i)
+	for i in list(comb):
+		list1.append(list(i))
+list3=[]
+for j in range(len(list1)):
+	list2=[]
+	m=len(list1[j])
+	for i in range(m-1,-1,-1):
+		list2.append(list1[j][i])
+	list3.append(list2)
+flag=0
+for i in range(0,len(list1)):
+	if list1[i]==list3[i]:
+		flag=len(list1[i])
+i=len(l)
+print(i-flag)
