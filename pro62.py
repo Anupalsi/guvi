639ebbd4b5355508f6438fca8f172c53b0ffce88
devikamadasamy <34593992+devikamadasamy@users.noreply.github.com>
2018-06-23 16:41:20 +0530

    Update pro62.py

--- pro62.py
+++ pro62.py
@@ -1,25 +1,22 @@
-stir=input()
-n=len(stir)
+from itertools import combinations
+l=input()
+m=list(l)
 list1=[]
-for i in range(2,n+1):
-	list1.append(stir[0:i])
-print(list1)
+for i in range(2,len(m)+1):
+	comb=combinations(m,i)
+	for i in list(comb):
+		list1.append(list(i))
 list3=[]
 for j in range(len(list1)):
 	list2=[]
 	m=len(list1[j])
 	for i in range(m-1,-1,-1):
 		list2.append(list1[j][i])
-	
-	a="".join(list2)
-	list3.append(a)
-print(list3)
+	list3.append(list2)
 flag=0
 for i in range(0,len(list1)):
 	if list1[i]==list3[i]:
-		flag=i
-if n==flag+2:
-	print(0)
-else:
-	w=stir[flag+2:]
-	print(len(w))
+		flag=len(list1[i])
+i=len(l)
+print(i-flag)
+	
