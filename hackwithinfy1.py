05ea94e52c267ff3260c39c58ab39e56eeac39de
devikamadasamy <34593992+devikamadasamy@users.noreply.github.com>
2018-07-09 20:21:05 +0530

    Update hackwithinfy1.py

--- hackwithinfy1.py
+++ hackwithinfy1.py
@@ -1,10 +1,8 @@
 import itertools
 def isposible(a,b,c,d):
 	flag=0
-	
 	lst=[]
-	p=max(c,d)
-	for i in range(2,p+1):
+	for i in range(2,20):
 		lst=lst+(list(map(list,(itertools.product([a,b], repeat=i)))))	
 	for i in lst:
 		if (i.count(a)>0 and i.count(b)>0):
